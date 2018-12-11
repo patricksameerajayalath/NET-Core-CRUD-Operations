9b570cc260e713c1ca068e8c8150c925f612da07
patricksameera <elakiri99@gmail.com>
2018-12-11 14:42:47 +1100

    Initial checkin

---
+++ NETCoreCRUDOperations/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace NETCoreCRUDOperations.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
